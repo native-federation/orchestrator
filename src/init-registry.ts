3da79c870581819ff5a531b56ac249f0085b0ca2
Aukevanoost <auke.v.o@hotmail.com>
2026-05-25 22:25:37 +0200

    chore: Prepared release 4.2.1

--- src/init-registry.ts
+++ src/init-registry.ts
@@ -1,6 +1,6 @@
 import { createRegistry } from 'lib/registry.index';
 import type { NFEventRegistry } from './lib/1.domain/registry/event-registry.contract';
-import type { NFEventRegistryConfig } from './lib/2.app/config/registry.contract';
+import type { NFEventRegistryOptions } from './lib/2.app/config/registry.contract';
 
 declare global {
   interface Window {
@@ -11,22 +11,22 @@ declare global {
 (function (): void {
   const script = document.currentScript as HTMLScriptElement | null;
 
-  const config: NFEventRegistryConfig = {
+  const options: NFEventRegistryOptions = {
     // Maximum number of different event types to keep in the registry.
-    maxStreams: script?.dataset?.maxStreams ? Number(script.dataset.maxStreams) : 50,
+    maxStreams: script?.dataset?.maxStreams ? Number(script.dataset.maxStreams) : undefined,
 
     // Maximum number of events to keep in the history for each event type.
-    maxEvents: script?.dataset?.maxEvents ? Number(script.dataset.maxEvents) : 50,
+    maxEvents: script?.dataset?.maxEvents ? Number(script.dataset.maxEvents) : undefined,
 
     // When trimming events, remove this percentage of the oldest events.
     // E.g. with 0.25 and maxEvents=50, when the 51st event is added, the oldest 12 events are removed,
     // leaving 38 events in the history.
     removePercentage: script?.dataset?.removePercentage
       ? Number(script.dataset.removePercentage) / 100
-      : 0.5,
+      : undefined,
   };
 
-  const registry = createRegistry(config);
+  const registry = createRegistry(options);
 
   window.__NF_REGISTRY__ = Object.freeze(registry());
 })();
